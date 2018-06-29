038c40a3987d2416ddc749f93d980dc9852a9cae
rita33cool1 <rita33cool1@yahoo.com.tw>
2018-06-29 17:04:56 +0800

    Split users/views.py and users/serializer.py to app/user

---
+++ app/user/serializer.py
@@ -0,0 +1,95 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+from ..models import UserProfile
+from rest_framework.authtoken.models import Token
+import re
+
+def email_check(email):
+    pattern = re.compile(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?")
+    return re.match(pattern, email)
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            label='Email',
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    username = serializers.CharField(
+            label='Username',
+            min_length=6,
+            max_length=32,
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    password = serializers.CharField(
+            label='Password', 
+            min_length=6, 
+            max_length=32,
+            required=True, 
+            write_only=True)
+   
+    def create(self, validated_data):
+        if not email_check(validated_data['email']):
+            raise serializers.ValidationError("Your email format is wrong!")
+        
+        user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'])
+        user_profile = UserProfile(user=user)
+        user_profile.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+            min_length=6,
+            max_length=32,
+            required=True
+            )
+    password = serializers.CharField(
+            min_length=6, 
+            max_length=32,
+            required=True
+            )
+    def validate_username(self, username):
+        print('validates username: ', username)
+            
+        filter_result = User.objects.filter(username__exact=username)
+        if not filter_result:
+            raise serializers.ValidationError("This username does not exist. Please register first.")
+
+        return username
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')    
+
+class TokenSerializer(serializers.ModelSerializer):
+    key = serializers.CharField(
+            label='token',
+            min_length=40,
+            max_length=40,
+            required=True
+            )
+    
+    def validate_key(self, key):
+        if not Token.objects.filter(key__exact=key):
+            raise serializers.ValidationError("This token does exist")
+        return key
+    
+    class Meta:
+        model = Token
+        fields = ('key','user_id')    
+
+class SetProfileSerializer(serializers.ModelSerializer):
+    expertises = serializers.ListField(
+            child=serializers.CharField(max_length=32),
+            )
+    
+    class Meta:
+        model = UserProfile
+        fields = (['expertises'])    
+
+
---
+++ app/user/views.py
@@ -0,0 +1,100 @@
+#from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from ..models import UserProfile
+from ..models import Expertise
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.http import HttpResponseNotFound
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
+from .serializer import LoginSerializer
+from .serializer import TokenSerializer
+from .serializer import SetProfileSerializer
+from rest_framework.authtoken.models import Token
+
+
+##--------------------API-------------------##
+success_message = 'Success'
+
+@api_view(['POST'])
+def UserCreate(request, format='json'):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        if user:
+            json = {'msg':success_message}
+            return Response(json, status=status.HTTP_201_CREATED)
+    json = {'msg':serializer.errors}
+    return Response(json, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+def UserLogin(request, format='json'):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        username = serializer.data['username']
+        password = serializer.data['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            success_message = success_message 
+            if not Token.objects.filter(user_id__exact=user.id):
+                token = Token.objects.create(user=user)
+            else:
+                token = Token.objects.get(user_id=user.id)
+            json = {'msg':success_message, 'token':token.key}
+            return Response(json, status=status.HTTP_202_ACCEPTED)
+    json = {'msg':serializer.errors}       
+    return Response(json, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def GetProfile(request, format='json'):
+    serializer = TokenSerializer(data=request.data)
+    if serializer.is_valid():
+        token_key = serializer.data['key']
+        token = Token.objects.get(key=token_key)
+        user = User.objects.get(id=token.user_id)
+        profile = UserProfile.objects.get(user_id=token.user_id)
+        exps = []
+        for exp in profile.expertises.all():
+            exps.append(exp.expertise)
+        json = {
+                'msg': success_message, 
+                'username': user.username, 
+                'email':user.email, 
+                'expertise':exps
+                }
+        return Response(json, status=status.HTTP_202_ACCEPTED)
+    
+    json = {'msg':serializer.errors}       
+    return Response(json, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def SetProfile(request, format='json'):
+    token_serializer = TokenSerializer(data=request.data)
+    profile_serializer = SetProfileSerializer(data=request.data)
+    if token_serializer.is_valid():
+        token_key = token_serializer.data['key']
+        token = Token.objects.get(key=token_key)
+        if profile_serializer.is_valid():
+            expertise_str = profile_serializer.data['expertises']
+            profile = UserProfile.objects.get(user_id=token.user_id)
+            profile.expertises.clear()
+            for exp in expertise_str:
+                exper = Expertise(expertise=exp)
+                exper.save()
+                profile.save()
+                profile.expertises.add(exper)
+            exps = []
+            for exp in profile.expertises.all():
+                 exps.append(exp.expertise)
+            json = {'msg': success_message, 'expertise':exps}
+            return Response(json, status=status.HTTP_202_ACCEPTED)
+    json = {'msg':token_serializer.errors+profile_serializer.errors}
+    return Response(json,status=status.HTTP_400_BAD_REQUEST)
+
